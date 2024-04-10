7fe703f91c29814216c4fdbbf991d8bf6756dd8a
Pranjal Shukla <pranjal.shukla@betaflux.co>
2024-04-10 15:10:33 +0200

    added another logic to read alphabets

--- index.js
+++ index.js
@@ -1,37 +1,61 @@
-const fs = require("fs")
+const fs = require("fs");
 
 fs.readFile("ort_problem_input.txt", "utf8", (err, data) => {
-    if (err) {
-        console.log(err)
-    } else {
-        //store words in array
-        const wordsArr = data.split("\n")
-        let numsArr = []
+  if (err) {
+    console.log(err);
+  } else {
+    //create an array of words
+    let numbers = [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+    ];
 
-        //iterate over each word
-        wordsArr.forEach(word => {
-            let pair = ""
-            const digitsInWord = []
-            //iterate over each alphabet in the word
-            for(let i = 0; i < word.length; i++) {
-                // if it is a number 
-              if(!isNaN(word[i])) {
-                digitsInWord.push(word[i]) 
-            }
+    //store words in array
+    const wordsArr = data.split("\n");
+  
+    let numsArr = [];
+
+    //iterate over each word
+    wordsArr.forEach((word) => {
+      let pair = "";
+      const digitsInWord = [];
+      let tempWord = ""
+      //iterate over each alphabet in the word
+      for (let i = 0; i < word.length; i++) {
+        // if it is a number
+        if (!isNaN(word[i])) {
+          digitsInWord.push(word[i]);  
         }
-            //form the pair and store it
-            if(digitsInWord.length > 0) {
-                pair = digitsInWord[0] + digitsInWord[digitsInWord.length - 1]
-                numsArr.push(Number(pair))
-            }
+        tempWord += word[i];  //nkone
+
+        numbers.forEach((num, index) => {
+          if(tempWord.includes(num)) {
+            digitsInWord.push(index + 1)
+            tempWord = word[i]
+          }
         })
+      }
 
-        //find the sum of all numbers
-        const sumOfAllDigits = numsArr.reduce((acc, curr) => {
-            return acc + curr
-        }, 0)
+      //form the pair and store it
+      if (digitsInWord.length > 0) {
+        pair = digitsInWord[0].toString() + digitsInWord[digitsInWord.length - 1].toString();
+        numsArr.push(Number(pair));
+      }
+    });
 
-        console.log("final sum is", sumOfAllDigits)
-        
-    }
-})
+    //find the sum of all numbers
+    const sumOfAllDigits = numsArr.reduce((acc, curr) => {
+      return acc + curr;
+    }, 0);
+
+    console.log("final sum is", sumOfAllDigits);
+  }
+
+});
